e609df3f8569297fcabff638fa55824898b2ae77
Jack Davies <jgdav6@student.monash.edu>
2018-05-14 18:22:10 +1000

    alternate solutions to 4Sum problem

---
+++ 18-2.py
@@ -0,0 +1,32 @@
+class Solution:
+    def fourSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        if len(nums) < 4:
+            return []
+        
+        nums.sort()
+        
+        ab_sum = {}
+        for _a in range(len(nums)):
+            for _b in range(_a+1, len(nums)):
+                ab = nums[_a] + nums[_b]
+                if ab in ab_sum:
+                    ab_sum[ab].append((_a, _b))
+                else:
+                    ab_sum[ab] = ([(_a, _b)])
+        
+        # a + b = target - (c + d)
+        solutions = set()
+        for _c in range(len(nums)):
+            for _d in range(_c+1, len(nums)):
+                t_cd = target - (nums[_c] + nums[_d])
+                if t_cd in ab_sum:
+                    for (_a, _b) in ab_sum[t_cd]:
+                        if _b < _c:
+                            solutions.add((nums[_a], nums[_b], nums[_c], nums[_d]))
+
+        return(list(solutions))
---
+++ 18-3.py
@@ -0,0 +1,34 @@
+class Solution:
+    def fourSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        if len(nums) < 4:
+            return []
+        
+        nums.sort()
+        n = len(nums)
+        
+        solutions = set()
+        for _a in range(n - 3):
+            for _b in range(_a + 1, n - 2):
+                _c, _d = _b + 1, n - 1
+                ab = nums[_a] + nums[_b]
+                # skip the following cases
+                if ((ab + nums[_c] + nums[_c + 1] > target) or 
+                    (ab + nums[_d] + nums[_d - 1] < target)):
+                    continue
+                
+                while _c < _d:
+                    t_cd = target - nums[_c] - nums[_d]
+                    if ab < t_cd:
+                        _c += 1
+                    elif ab > t_cd:
+                        _d -= 1
+                    else:
+                        solutions.add((nums[_a], nums[_b], nums[_c], nums[_d]))
+                        _c += 1
+        
+        return list(solutions)
